e81e8b6ab5dc9939a5ed4276c71f185b81d95eaa
Agustin Barto <abarto@machinalis.com>
2016-05-10 20:40:20 -0300

    Move cache classes. Add unit tests

--- splitio/splits.py
+++ splitio/splits.py
@@ -371,23 +371,6 @@ class SelfRefreshingSplitFetcher(InMemorySplitFetcher):
             self._stopped = True
 
 
-class CacheBasedSplitFetcher(SplitFetcher):
-    def __init__(self, split_cache):
-        """
-        A cache based SplitFetcher implementation
-        :param split_cache: The split cache
-        :type split_cache: SplitCache
-        :param split_parser: The split parser
-        :type split_parser: SplitParser
-        """
-        super(SelfRefreshingSplitFetcher, self).__init__()
-
-        self._split_cache = split_cache
-
-    def fetch(self, feature):
-        return self._split_cache.get_split(feature)
-
-
 class SplitChangeFetcher(object):
     def __init__(self):
         """Fetches changes in splits since a reference point."""
