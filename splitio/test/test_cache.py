5144fc229e0bcdfe57d922a05df4e501bbe9b629
Agustin Barto <abarto@machinalis.com>
2016-05-17 14:02:12 -0300

    Changed default implementations to make the dummy. Move split and segment code back to their modules.

--- splitio/test/test_cache.py
+++ splitio/test/test_cache.py
@@ -9,8 +9,7 @@ except ImportError:
 
 from unittest import TestCase
 
-from splitio.cache import (InMemorySplitCache, InMemorySegmentCache, InMemoryImpressionsCache,
-                           CacheBasedSegment, CacheBasedSegmentFetcher, CacheBasedSplitFetcher)
+from splitio.cache import (InMemorySplitCache, InMemorySegmentCache, InMemoryImpressionsCache)
 from splitio.test.utils import MockUtilsMixin
 
 
@@ -20,7 +19,7 @@ class InMemorySegmentCacheTests(TestCase, MockUtilsMixin):
         self.some_change_number = mock.MagicMock()
         self.segment_cache = InMemorySegmentCache()
         self.entries_mock = self.patch_object(self.segment_cache, '_entries')
-        self.existing_entries = {'key_set': {'some_key_1', 'some_key_2'}}
+        self.existing_entries = {'key_set': {'some_key_1', 'some_key_2'}, 'change_number': -1}
         self.entries_mock.__getitem__.return_value = self.existing_entries
 
     def test_add_keys_to_segment_sets_keys_to_union(self):
@@ -28,11 +27,6 @@ class InMemorySegmentCacheTests(TestCase, MockUtilsMixin):
         self.segment_cache.add_keys_to_segment(self.some_segment_name, {'some_key_2', 'some_key_3'})
         self.assertSetEqual({'some_key_1', 'some_key_2', 'some_key_3'},
                             self.existing_entries['key_set'])
-
-    def test_set_segment_keys_sets_keys(self):
-        """Tests that set_segment_keys sets keys for a segment"""
-        self.segment_cache.set_segment_keys(self.some_segment_name, {'some_key_2', 'some_key_3'})
-        self.assertSetEqual({'some_key_2', 'some_key_3'}, self.existing_entries['key_set'])
 
     def test_remove_keys_from_segment_set_keys_to_difference(self):
         """Tests that remove_from_segment sets keys to difference of old and new keys"""
@@ -45,16 +39,16 @@ class InMemorySegmentCacheTests(TestCase, MockUtilsMixin):
         self.assertTrue(self.segment_cache.is_in_segment(self.some_segment_name, 'some_key_1'))
         self.assertFalse(self.segment_cache.is_in_segment(self.some_segment_name, 'some_key_3'))
 
-    def test_set_change_number_sets_change_number(self):
-        """Test that set_change_number sets the change number"""
-        self.segment_cache.set_change_number(self.some_change_number)
-        self.assertEqual(self.some_change_number, self.segment_cache._change_number)
+    def test_set_change_number_sets_change_number_for_segment(self):
+        """Tests that set_change_number sets the change number for the segment"""
+        self.segment_cache.set_change_number(self.some_segment_name, self.some_change_number)
+        self.assertIn('change_number', self.existing_entries)
+        self.assertEqual(self.some_change_number, self.existing_entries['change_number'])
 
-    def test_get_change_number_returns_change_number(self):
-        """Test that get_change_number returns the change number"""
-        self.segment_cache.set_change_number(self.some_change_number)
-        self.segment_cache._change_number = self.some_change_number
-        self.assertEqual(self.some_change_number, self.segment_cache.get_change_number())
+    def test_get_change_number_returns_existing_change_number(self):
+        """Tests that get_change_number resturns the current change number for the segment"""
+        self.assertEqual(self.existing_entries['change_number'],
+                         self.segment_cache.get_change_number(self.some_segment_name))
 
 
 class InMemorySplitCacheTests(TestCase, MockUtilsMixin):
@@ -98,6 +92,19 @@ class InMemorySplitCacheTests(TestCase, MockUtilsMixin):
         self.assertEqual(self.some_change_number, self.split_cache.get_change_number())
 
 
+class InMemoryImpressionsCacheInitTests(TestCase, MockUtilsMixin):
+    def setUp(self):
+        self.some_impressions = mock.MagicMock()
+        self.impressions_mock = mock.MagicMock()
+        self.defaultdict_mock = self.patch('splitio.cache.defaultdict',
+                                           return_value=self.impressions_mock)
+
+    def test_init_initializes_impressions_with_impressions_parameter(self):
+        """Tests that __init__ updates the _impressions field with the impressions parameter"""
+        InMemoryImpressionsCache(impressions=self.some_impressions)
+        self.impressions_mock.update.assert_called_once_with(self.some_impressions)
+
+
 class InMemoryImpressionsCacheTests(TestCase, MockUtilsMixin):
     def setUp(self):
         self.some_impression = mock.MagicMock()
@@ -138,52 +145,3 @@ class InMemoryImpressionsCacheTests(TestCase, MockUtilsMixin):
         result = self.impressions_cache.fetch_all_and_clear()
         self.deepcopy_mock.assert_called_once_with(self.impressions_mock)
         self.assertEqual(self.deepcopy_mock.return_value, result)
-
-
-class CacheBasedSegmentFetcherTests(TestCase, MockUtilsMixin):
-    def setUp(self):
-        self.some_segment_name = mock.MagicMock()
-        self.some_segment_cache = mock.MagicMock()
-        self.segment_fetcher = CacheBasedSegmentFetcher(self.some_segment_cache)
-
-    def test_fetch_creates_cache_based_segment(self):
-        segment = self.segment_fetcher.fetch(self.some_segment_name)
-        self.assertIsInstance(segment, CacheBasedSegment)
-        self.assertEqual(self.some_segment_cache, segment._segment_cache)
-        self.assertEqual(self.some_segment_name, segment.name)
-
-
-class CacheBasedSegmentTests(TestCase, MockUtilsMixin):
-    def setUp(self):
-        self.some_key = mock.MagicMock()
-        self.some_name = mock.MagicMock()
-        self.some_segment_cache = mock.MagicMock()
-        self.segment = CacheBasedSegment(self.some_name, self.some_segment_cache)
-
-    def test_contains_calls_segment_cache_is_in_segment(self):
-        """Test that contains calls segment_cache is_in_segment method"""
-        self.segment.contains(self.some_key)
-        self.some_segment_cache.is_in_segment.assert_called_once_with(self.some_name,
-                                                                      self.some_key)
-
-    def test_contains_returns_segment_cache_is_in_segment_results(self):
-        """Test that contains returns the result of calling segment_cache is_in_segment method"""
-        self.assertEqual(self.some_segment_cache.is_in_segment.return_value,
-                         self.segment.contains(self.some_key))
-
-
-class CacheBasedSplitFetcherTests(TestCase):
-    def setUp(self):
-        self.some_feature = mock.MagicMock()
-        self.some_split_cache = mock.MagicMock()
-        self.split_fetcher = CacheBasedSplitFetcher(split_cache=self.some_split_cache)
-
-    def test_fetch_calls_get_split(self):
-        """Test that fetch calls get_split on the split cache"""
-        self.split_fetcher.fetch(self.some_feature)
-        self.some_split_cache.get_split.assert_called_once_with(self.some_feature)
-
-    def test_fetch_results_get_split_result(self):
-        """Test that fetch returns the result of calling get split on the cache"""
-        self.assertEqual(self.some_split_cache.get_split.return_value,
-                         self.split_fetcher.fetch(self.some_feature))
