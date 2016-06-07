4d2e1829fd2f993e7607c68d536c6cae54e934eb
Agustin Barto <abarto@machinalis.com>
2016-06-07 12:06:30 -0300

    Remove reference to settings module

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 from setuptools import setup
 from sys import version_info
-from splitio.settings import PACKAGE_VERSION
 
 tests_require = ['flake8', 'nose', 'coverage']
 install_requires = ['arrow>=0.7.0', 'requests>=2.9.1', 'future>=0.15.2']
@@ -12,12 +11,12 @@ if version_info < (3,):
     install_requires += ['six>=1.10.0', 'futures>=3.0.5', 'enum34>=1.1.5']
 
 setup(name='splitio-client',
-      version=PACKAGE_VERSION,
+      version='0.0.1',
       description='Split.io Python Client',
       author='Patricio Echague',
       author_email='pato@split.io',
       url='https://github.com/splitio/python-client',
-      download_url='https://github.com/splitio/python-client/tarball/{}'.format(PACKAGE_VERSION),
+      download_url='https://github.com/splitio/python-client/tarball/0.0.1',
       license='Apache License 2.0',
       install_requires=install_requires,
       tests_require=tests_require,
