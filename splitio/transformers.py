4242a34ff6d2479618b085664cd72b0c8157d686
Agustin Barto <abarto@machinalis.com>
2016-06-08 16:10:55 -0300

    SPLIT-973 Fix transformers to return None with invalid values

--- splitio/transformers.py
+++ splitio/transformers.py
@@ -1,12 +1,26 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import logging
+
 from builtins import int
 
 from arrow import Arrow
 
+logger = logging.getLogger(__name__)
+
 
 class TransformMixin(object):
     """Base for all transform mixins"""
+    def _transform(self, value):
+        """
+        Transforms a value. Subclasses need to implement this method
+        :param value: The value to transform
+        :type value: any
+        :return: The transformed value
+        :rtype: any
+        """
+        raise NotImplementedError()
+
     def transform(self, value):
         """
         Transforms a value
@@ -15,12 +29,15 @@ class TransformMixin(object):
         :return: The transformed value
         :rtype: any
         """
-        raise NotImplementedError()
+        try:
+            return self._transform(value)
+        except:
+            logger.exception('Exception caught transforming value. value = %s', value)
 
 
 class AsNumberTransformMixin(TransformMixin):
     """Mixin to allow transforming values to int (long)"""
-    def transform(self, value):
+    def _transform(self, value):
         """
         Transforms value to int (long in Python2)
         :param value: Any value suitable to be transformed
@@ -36,7 +53,7 @@ class AsNumberTransformMixin(TransformMixin):
 
 class AsDateHourMinuteTimestampTransformMixin(TransformMixin):
     """Mixin to allow truncating timestamp to the minute"""
-    def transform(self, value):
+    def _transform(self, value):
         """
         Truncates seconds and milliseconds from a long value of milliseconds from epoch
         :param value: An int value representing the number of milliseconds from epoch (timestmap)
@@ -53,7 +70,7 @@ class AsDateHourMinuteTimestampTransformMixin(TransformMixin):
 
 class AsDateTimestampTransformMixin(TransformMixin):
     """Mixin to allow truncating timestamp to midnight"""
-    def transform(self, value):
+    def _transform(self, value):
         """
         Truncates seconds and milliseconds from a long value of milliseconds from epoch
         :param value: An int value representing the number of milliseconds from epoch (timestmap)
