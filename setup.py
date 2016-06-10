215b7a00125d0d50da579fd8f681bda8a03176f8
Agustin Barto <abarto@machinalis.com>
2016-06-10 12:03:38 -0300

    Flake8 Fixes

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ if version_info < (3,):
     install_requires += ['six>=1.10.0', 'futures>=3.0.5', 'enum34>=1.1.5']
 
 with open(path.join(path.abspath(path.dirname(__file__)), 'splitio', 'version.py')) as f:
-    exec (f.read())
+    exec(f.read())
 
 setup(name='splitio_client',
-      version=__version__,
+      version=__version__,  # noqa
       description='Split.io Python Client',
       author='Patricio Echague',
       author_email='pato@split.io',
