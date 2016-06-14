a1f762fc6aaaf24a6a5600a3aefd13727a1fef37
Agustin Barto <abarto@machinalis.com>
2016-06-14 16:15:34 -0300

    Fix unit tests

--- splitio/tasks.py
+++ splitio/tasks.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 
 from .splits import Status
+from .impressions import build_impressions_data
 
 _logger = logging.getLogger(__name__)
 
@@ -103,3 +104,49 @@ def update_splits(split_cache, split_change_fetcher, split_parser):
     except:
         _logger.exception('Exception caught updating split definitions')
         split_cache.disable()
+
+
+def report_impressions(impressions_cache, sdk_api):
+    """If the reporting process is enabled (through the impressions cache), this function collects
+    the impressions from the cache and sends them to Split through the events API. If the process
+    fails, no exceptions are raised (but they are logged) and the process is disabled.
+    """
+    try:
+        if not impressions_cache.is_enabled():
+            return
+
+        impressions = impressions_cache.fetch_all_and_clear()
+        test_impressions_data = build_impressions_data(impressions)
+
+        if len(test_impressions_data) > 0:
+            _logger.info('Posting impressions for features: %s.', ', '.join(impressions.keys()))
+            sdk_api.test_impressions(test_impressions_data)
+    except:
+        _logger.exception('Exception caught report impressions. Disabling impressions log.')
+        impressions_cache.disable()
+
+
+def report_metrics(metrics_cache, sdk_api):
+    """If the reporting process is enabled (through the metrics cache), this function collects
+    the time, count and gauge from the cache and sends them to Split through the events API. If the
+    process fails, no exceptions are raised (but they are logged) and the process is disabled."""
+    try:
+        if not metrics_cache.is_enabled():
+            return
+
+        metrics = metrics_cache.fetch_all_and_clear()
+
+        if 'time' in metrics and len(metrics['time']) > 0:
+            _logger.info('Sending times metrics...')
+            sdk_api.metrics_times(metrics['time'])
+
+        if 'count' in metrics and len(metrics['count']) > 0:
+            _logger.info('Sending counters metrics...')
+            sdk_api.metrics_counters(metrics['count'])
+
+        if 'gauge' in metrics and len(metrics['gauge']) > 0:
+            _logger.info('Sending gauge metrics...')
+            sdk_api.metrics_gauge(metrics['gauge'])
+    except:
+        _logger.exception('Exception caught reporting metrics')
+        metrics_cache.disable()
