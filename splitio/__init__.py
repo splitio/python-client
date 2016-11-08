4adc86faa09faec650e64e665035c18d4831e136
Sebastian Arrubia <sarrubia@gmail.com>
2016-11-08 18:26:29 -0300

    Adding Key

--- splitio/__init__.py
+++ splitio/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from .factories import get_factory # noqa
-from .clients import get_client, get_redis_client  # noqa
+from .clients import get_client, get_redis_client, Key  # noqa
 from .version import __version__  # noqa
 
 __all__ = ('api', 'cache', 'clients', 'matchers', 'segments', 'settings', 'splits', 'splitters',
