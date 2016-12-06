8ed71850db96c76f59163c6650e7ce91af932a74
Sebastian Arrubia <sarrubia@gmail.com>
2016-12-06 17:42:04 -0300

    Release verison 2.0.5

--- splitio/tests/test_clients.py
+++ splitio/tests/test_clients.py
@@ -426,7 +426,25 @@ class SelfRefreshingClientInitConfigTests(TestCase, MockUtilsMixin):
             'redisHost': 'localhost',
             'redisPort': 6379,
             'redisDb': 0,
-            'redisPassword':None
+            'redisPassword':None,
+            'redisSocketTimeout': None,
+            'redisSocketConnectTimeout': None,
+            'redisSocketKeepalive': None,
+            'redisSocketKeepaliveOptions': None,
+            'redisConnectionPool': None,
+            'redisUnixSocketPath': None,
+            'redisEncoding': 'utf-8',
+            'redisEncodingErrors': 'strict',
+            'redisCharset': None,
+            'redisErrors': None,
+            'redisDecodeResponses': False,
+            'redisRetryOnTimeout': False,
+            'redisSsl': False,
+            'redisSslKeyfile': None,
+            'redisSslCertfile': None,
+            'redisSslCertReqs': None,
+            'redisSslCaCerts': None,
+            'redisMaxConnections': None
         }
 
 
