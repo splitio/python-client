34c0b1d4b739d08fafd11fd1d92f56a205aa7232
Matias Melograno <matiasmelograno@split.io>
2018-09-11 18:45:56 -0300

    fixed issue for python 2

--- splitio/factories.py
+++ splitio/factories.py
@@ -42,7 +42,10 @@ class MainSplitFactory(SplitFactory):
 
         labels_enabled = config.get('labelsEnabled', True)
 
-        impression_listener = ImpressionListenerWrapper(config.get('impressionListener')) if 'impressionsListener' in config else None # noqa: E501,E261
+        if (config.get('impressionListener')):
+            impression_listener = ImpressionListenerWrapper(config.get('impressionListener'))
+        else:
+            impression_listener = None
 
         if 'redisHost' in config or 'redisSentinels' in config:
             broker = get_redis_broker(api_key, **kwargs)
