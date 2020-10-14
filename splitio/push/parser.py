dede4e52b67bd68a33b3aa41a8605a0546f2cfab
Matias Melograno <matiasmelograno@split.io>
2020-10-14 18:30:51 -0300

    updated incoming payload

--- splitio/push/parser.py
+++ splitio/push/parser.py
@@ -31,16 +31,23 @@ def parse_incoming_event(raw_event):
         return None
     
     try:
-        parsed_json = json.loads(raw_event)
-        if parsed_json is None:
+        parsed_raw_event = json.loads(raw_event)
+        if parsed_raw_event is None:
             return None
 
-        if 'statusCode' in parsed_json and 'code' in parsed_json and 'message' in parsed_json and 'href' in parsed_json:
-            return AblyError(parsed_json['code'], parsed_json['statusCode'], parsed_json['message'], parsed_json['href'])
-        elif 'name' in parsed_json and parsed_json['name'] == TAG_OCCUPANCY and 'data' in parsed_json and 'channel' in parsed_json:
-            return Occupancy(parsed_json['data'], parsed_json['channel'])
-        elif 'data' in parsed_json and 'channel' in parsed_json:
-            return Update(parsed_json['data'], parsed_json['channel'])
+        if not 'event' in parsed_raw_event or not 'data' in parsed_raw_event:
+            return None
+
+        parsed_data = json.loads(parsed_raw_event['data'])
+
+        if parsed_raw_event['event'] == 'error':
+            if 'statusCode' in parsed_data and 'code' in parsed_data and 'message' in parsed_data and 'href' in parsed_data:
+                return AblyError(parsed_data['code'], parsed_data['statusCode'], parsed_data['message'], parsed_data['href'])
+        elif parsed_raw_event['event'] == 'message':
+            if 'name' in parsed_data and parsed_data['name'] == TAG_OCCUPANCY and 'data' in parsed_data and 'channel' in parsed_data:
+                return Occupancy(parsed_data['data'], parsed_data['channel'])
+            elif 'data' in parsed_data and 'channel' in parsed_data:
+                return Update(parsed_data['data'], parsed_data['channel'])
         return None
     except ValueError:
         raise ValueError('Cannot parse json.')
--- tests/push/test_parser.py
+++ tests/push/test_parser.py
@@ -5,9 +5,16 @@ from splitio.push.parser import parse_incoming_event, Update, AblyError, Occupan
 
 
 def wrap_json(channel, data):
-    base = '{{"channel":"{channel}","data":"{data}","id":"ZlalwoKlXW:0:0","clientId":"pri:MzIxMDYyOTg5MA==","timestamp":1591996755043,"encoding":"json"}}'
-    return base.format(channel=channel, data=data)
-
+    return json.dumps({
+        'data': json.dumps({
+            'id':'ZlalwoKlXW:0:0',
+            'timestamp':1591996755043,
+            'encoding':'json',
+            'channel': channel,
+            'data': json.dumps(data)
+        }),
+        'event': 'message'
+    })
 
 class ParserTests(object):
     """Parser tests."""
@@ -17,7 +24,7 @@ class ParserTests(object):
         assert parse_incoming_event(None) is None
         assert parse_incoming_event('') is None
         assert parse_incoming_event('  ') is None
-        assert parse_incoming_event('{}') is None
+        assert parse_incoming_event(json.dumps({})) is None
 
         with pytest.raises(ValueError):
             parse_incoming_event('asd')
@@ -26,28 +33,50 @@ class ParserTests(object):
         """Test parse Update event."""
         e0 = wrap_json(
             'NDA5ODc2MTAyNg==_MzAyODY0NDkyOA==_splits',
-            "{'type':'SPLIT_KILL','changeNumber':1591996754396,'defaultTreatment':'some','splitName':'test'}",
+            {'type':'SPLIT_KILL','changeNumber':1591996754396,'defaultTreatment':'some','splitName':'test'},
         )
         assert isinstance(parse_incoming_event(e0), Update)
 
         e1 = wrap_json(
             'NDA5ODc2MTAyNg==_MzAyODY0NDkyOA==_splits',
-            "{'type':'SPLIT_UPDATE','changeNumber':1591996685190}",
+            {'type':'SPLIT_UPDATE','changeNumber':1591996685190},
         )
         assert isinstance(parse_incoming_event(e1), Update)
     
         e2 = wrap_json(
             'NDA5ODc2MTAyNg==_MzAyODY0NDkyOA==_segments',
-            "{'type':'SEGMENT_UPDATE','changeNumber':1591988398533,'segmentName':'some'}",
+            {'type':'SEGMENT_UPDATE','changeNumber':1591988398533,'segmentName':'some'},
         )
         assert isinstance(parse_incoming_event(e2), Update)
 
     def test_error_parsing(self):
         """Test parse AblyError event."""
-        e0 = '{"code":40142,"message":"Token expired","statusCode":401,"href":"https://help.io/error/40142","timestamp":1591996755043,"encoding":"json"}'
+        e0 = json.dumps({
+            'data': json.dumps({
+                'code': 40142,
+                'message': 'Token expired',
+                'statusCode': 401,
+                'href': 'https://help.io/error/40142',
+            }),
+            'event': 'error'
+        })
         assert isinstance(parse_incoming_event(e0), AblyError)
     
     def test_occupancy_parsing(self):
         """Test parse Occupancy event."""
-        e0 = '{"channel":"[?occupancy=metrics.publishers]control_sec","data":"{\\"metrics\\":{\\"publishers\\":1}}","id":"ZlalwoKlXW:0:0","clientId":"pri:MzIxMDYyOTg5MA==","timestamp":1591996755043,"encoding":"json","name":"[meta]occupancy"}'
+        e0 = json.dumps({
+            'data': json.dumps({
+                'id':'ZlalwoKlXW:0:0',
+                'timestamp':1591996755043,
+                'encoding':'json',
+                'channel': '[?occupancy=metrics.publishers]control_sec',
+                'data': json.dumps({
+                    'metrics': json.dumps({
+                        'publishers': 1
+                    }),
+                }),
+                'name': '[meta]occupancy',
+            }),
+            'event': 'message'
+        })
         assert isinstance(parse_incoming_event(e0), Occupancy)
