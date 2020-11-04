1069ad380792d88d9ff19d6c8aeada026e6a0468
Martin Redolatti <martinredolatti@gmail.com>
2020-11-03 23:44:27 -0300

    more tests

--- tests/integration/test_streaming_e2e.py
+++ tests/integration/test_streaming_e2e.py
@@ -1,6 +1,6 @@
 """Streaming integration tests."""
 # pylint:disable=no-self-use,invalid-name,too-many-arguments,too-few-public-methods,line-too-long
-# pylint:disable=too-many-statements,too-many-locals
+# pylint:disable=too-many-statements,too-many-locals,too-many-lines
 import threading
 import time
 import json
@@ -404,6 +404,18 @@ class StreamingIntegrationTests(object):
         assert req.path == '/api/splitChanges?since=4'
         assert req.headers['authorization'] == 'Bearer some_apikey'
 
+        # Split kill
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=4'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Iteration until since == till
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=5'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
         # Cleanup
         destroy_event = threading.Event()
         factory.destroy(destroy_event)
@@ -781,6 +793,228 @@ class StreamingIntegrationTests(object):
         sse_server.publish(sse_server.GRACEFUL_REQUEST_END)
         sse_server.stop()
         split_backend.stop()
+
+    def test_server_closes_connection(self):
+        """Test that if the server closes the connection, the whole flow is retried with BO."""
+        auth_server_response = {
+            'pushEnabled': True,
+            'token': ('eyJhbGciOiJIUzI1NiIsImtpZCI6IjVZOU05US45QnJtR0EiLCJ0eXAiOiJKV1QifQ.'
+                      'eyJ4LWFibHktY2FwYWJpbGl0eSI6IntcIk1UWXlNVGN4T1RRNE13PT1fTWpBNE16Y3pO'
+                      'RFUxTWc9PV9zZWdtZW50c1wiOltcInN1YnNjcmliZVwiXSxcIk1UWXlNVGN4T1RRNE13P'
+                      'T1fTWpBNE16Y3pORFUxTWc9PV9zcGxpdHNcIjpbXCJzdWJzY3JpYmVcIl0sXCJjb250cm'
+                      '9sX3ByaVwiOltcInN1YnNjcmliZVwiLFwiY2hhbm5lbC1tZXRhZGF0YTpwdWJsaXNoZXJ'
+                      'zXCJdLFwiY29udHJvbF9zZWNcIjpbXCJzdWJzY3JpYmVcIixcImNoYW5uZWwtbWV0YWRh'
+                      'dGE6cHVibGlzaGVyc1wiXX0iLCJ4LWFibHktY2xpZW50SWQiOiJjbGllbnRJZCIsImV4c'
+                      'CI6MTYwNDEwMDU5MSwiaWF0IjoxNjA0MDk2OTkxfQ.aP9BfR534K6J9h8gfDWg_CQgpz5E'
+                      'vJh17WlOlAKhcD0')
+        }
+
+        split_changes = {
+            -1: {
+                'since': -1,
+                'till': 1,
+                'splits': [make_simple_split('split1', 1, True, False, 'on', 'user', True)]
+            },
+            1: {
+                'since': 1,
+                'till': 1,
+                'splits': []
+            }
+        }
+
+        segment_changes = {}
+        split_backend_requests = Queue()
+        split_backend = SplitMockServer(split_changes, segment_changes, split_backend_requests,
+                                        auth_server_response)
+        sse_requests = Queue()
+        sse_server = SSEMockServer(sse_requests)
+
+        split_backend.start()
+        sse_server.start()
+        sse_server.publish(make_initial_event())
+        sse_server.publish(make_occupancy('control_pri', 2))
+        sse_server.publish(make_occupancy('control_sec', 2))
+
+        kwargs = {
+            'sdk_api_base_url': 'http://localhost:%d/api' % split_backend.port(),
+            'events_api_base_url': 'http://localhost:%d/api' % split_backend.port(),
+            'auth_api_base_url': 'http://localhost:%d/api' % split_backend.port(),
+            'streaming_api_base_url': 'http://localhost:%d' % sse_server.port(),
+            'config': {'connectTimeout': 10000, 'featuresRefreshRate': 100,
+                       'segmentsRefreshRate': 100, 'metricsRefreshRate': 100,
+                       'impressionsRefreshRate': 100, 'eventsPushRate': 100}
+        }
+
+        factory = get_factory('some_apikey', **kwargs)
+        factory.block_until_ready(1)
+        assert factory.ready
+        assert factory.client().get_treatment('maldo', 'split1') == 'on'
+        task = factory._sync_manager._synchronizer._split_tasks.split_task._task  # pylint:disable=protected-access
+        assert not task.running()
+
+        time.sleep(1)
+        split_changes[1] = {
+            'since': 1,
+            'till': 2,
+            'splits': [make_simple_split('split1', 2, True, False, 'off', 'user', False)]
+        }
+        split_changes[2] = {'since': 2, 'till': 2, 'splits': []}
+        sse_server.publish(make_split_change_event(2))
+        time.sleep(1)
+        assert factory.client().get_treatment('maldo', 'split1') == 'off'
+
+        sse_server.publish(SSEMockServer.GRACEFUL_REQUEST_END)
+        time.sleep(1)
+        assert factory.client().get_treatment('maldo', 'split1') == 'off'
+        assert task.running()
+
+        time.sleep(2)  # wait for the backoff to expire so streaming gets re-attached
+
+        # re-send initial event AND occupancy
+        sse_server.publish(make_initial_event())
+        sse_server.publish(make_occupancy('control_pri', 2))
+        sse_server.publish(make_occupancy('control_sec', 2))
+        time.sleep(2)
+
+        assert not task.running()
+        split_changes[2] = {
+            'since': 2,
+            'till': 3,
+            'splits': [make_simple_split('split1', 3, True, False, 'off', 'user', True)]
+        }
+        split_changes[3] = {'since': 3, 'till': 3, 'splits': []}
+        sse_server.publish(make_split_change_event(3))
+        time.sleep(1)
+        assert factory.client().get_treatment('maldo', 'split1') == 'on'
+        assert not task.running()
+
+        # Validate the SSE requests
+        sse_request = sse_requests.get()
+        assert sse_request.method == 'GET'
+        path, qs = sse_request.path.split('?', 1)
+        assert path == '/event-stream'
+        qs = parse_qs(qs)
+        assert qs['accessToken'][0] == (
+            'eyJhbGciOiJIUzI1NiIsImtpZCI6IjVZOU05'
+            'US45QnJtR0EiLCJ0eXAiOiJKV1QifQ.eyJ4LWFibHktY2FwYWJpbGl0eSI6IntcIk1UW'
+            'XlNVGN4T1RRNE13PT1fTWpBNE16Y3pORFUxTWc9PV9zZWdtZW50c1wiOltcInN1YnNjc'
+            'mliZVwiXSxcIk1UWXlNVGN4T1RRNE13PT1fTWpBNE16Y3pORFUxTWc9PV9zcGxpdHNcI'
+            'jpbXCJzdWJzY3JpYmVcIl0sXCJjb250cm9sX3ByaVwiOltcInN1YnNjcmliZVwiLFwiY'
+            '2hhbm5lbC1tZXRhZGF0YTpwdWJsaXNoZXJzXCJdLFwiY29udHJvbF9zZWNcIjpbXCJzd'
+            'WJzY3JpYmVcIixcImNoYW5uZWwtbWV0YWRhdGE6cHVibGlzaGVyc1wiXX0iLCJ4LWFib'
+            'HktY2xpZW50SWQiOiJjbGllbnRJZCIsImV4cCI6MTYwNDEwMDU5MSwiaWF0IjoxNjA0M'
+            'Dk2OTkxfQ.aP9BfR534K6J9h8gfDWg_CQgpz5EvJh17WlOlAKhcD0'
+        )
+
+        assert set(qs['channels'][0].split(',')) == set(['MTYyMTcxOTQ4Mw==_MjA4MzczNDU1Mg==_splits',
+                                                         'MTYyMTcxOTQ4Mw==_MjA4MzczNDU1Mg==_segments',
+                                                         '[?occupancy=metrics.publishers]control_pri',
+                                                         '[?occupancy=metrics.publishers]control_sec'])
+        assert qs['v'][0] == '1.1'
+
+        sse_request = sse_requests.get()
+        assert sse_request.method == 'GET'
+        path, qs = sse_request.path.split('?', 1)
+        assert path == '/event-stream'
+        qs = parse_qs(qs)
+        assert qs['accessToken'][0] == (
+            'eyJhbGciOiJIUzI1NiIsImtpZCI6IjVZOU05'
+            'US45QnJtR0EiLCJ0eXAiOiJKV1QifQ.eyJ4LWFibHktY2FwYWJpbGl0eSI6IntcIk1UW'
+            'XlNVGN4T1RRNE13PT1fTWpBNE16Y3pORFUxTWc9PV9zZWdtZW50c1wiOltcInN1YnNjc'
+            'mliZVwiXSxcIk1UWXlNVGN4T1RRNE13PT1fTWpBNE16Y3pORFUxTWc9PV9zcGxpdHNcI'
+            'jpbXCJzdWJzY3JpYmVcIl0sXCJjb250cm9sX3ByaVwiOltcInN1YnNjcmliZVwiLFwiY'
+            '2hhbm5lbC1tZXRhZGF0YTpwdWJsaXNoZXJzXCJdLFwiY29udHJvbF9zZWNcIjpbXCJzd'
+            'WJzY3JpYmVcIixcImNoYW5uZWwtbWV0YWRhdGE6cHVibGlzaGVyc1wiXX0iLCJ4LWFib'
+            'HktY2xpZW50SWQiOiJjbGllbnRJZCIsImV4cCI6MTYwNDEwMDU5MSwiaWF0IjoxNjA0M'
+            'Dk2OTkxfQ.aP9BfR534K6J9h8gfDWg_CQgpz5EvJh17WlOlAKhcD0'
+        )
+
+        assert set(qs['channels'][0].split(',')) == set(['MTYyMTcxOTQ4Mw==_MjA4MzczNDU1Mg==_splits',
+                                                         'MTYyMTcxOTQ4Mw==_MjA4MzczNDU1Mg==_segments',
+                                                         '[?occupancy=metrics.publishers]control_pri',
+                                                         '[?occupancy=metrics.publishers]control_sec'])
+        assert qs['v'][0] == '1.1'
+
+        # Initial apikey validation
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/segmentChanges/__SOME_INVALID_SEGMENT__?since=-1'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Initial splits fetch
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=-1'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Iteration until since == till
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=1'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Auth
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/auth'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # SyncAll after streaming connected
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=1'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Fetch after first notification
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=1'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Iteration until since == till
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=2'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # SyncAll on retryable error handling
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=2'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Auth after connection breaks
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/auth'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # SyncAll after streaming connected again
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=2'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Fetch after new notification
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=2'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Iteration until since == till
+        req = split_backend_requests.get()
+        assert req.method == 'GET'
+        assert req.path == '/api/splitChanges?since=3'
+        assert req.headers['authorization'] == 'Bearer some_apikey'
+
+        # Cleanup
+        destroy_event = threading.Event()
+        factory.destroy(destroy_event)
+        destroy_event.wait()
+        sse_server.publish(sse_server.GRACEFUL_REQUEST_END)
+        sse_server.stop()
+        split_backend.stop()
+
 
 def make_split_change_event(change_number):
     """Make a split change event."""
