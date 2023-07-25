6b6532aa819058a8bdb0a32d391cdb182f5c0a23
Bilal Al-Shahwany <bilal@split.io>
2023-07-25 11:48:34 -0700

    added asynctask async class

--- tests/tasks/util/test_asynctask.py
+++ tests/tasks/util/test_asynctask.py
@@ -2,8 +2,10 @@
 
 import time
 import threading
-from splitio.tasks.util import asynctask
+import pytest
 
+from splitio.tasks.util import asynctask
+from splitio.optional.loaders import asyncio
 
 class AsyncTaskTests(object):
     """AsyncTask test cases."""
@@ -115,4 +117,142 @@ class AsyncTaskTests(object):
         assert on_init.mock_calls == [mocker.call()]
         assert on_stop.mock_calls == [mocker.call()]
         assert len(main_func.mock_calls) == 2
+        assert not task.running()
+
+
+class AsyncTaskAsyncTests(object):
+    """AsyncTask test cases."""
+
+    @pytest.mark.asyncio
+    async def test_default_task_flow(self, mocker):
+        """Test the default execution flow of an asynctask."""
+        self.main_called = 0
+        async def main_func():
+            self.main_called += 1
+
+        self.init_called = 0
+        async def on_init():
+            self.init_called += 1
+
+        self.stop_called = 0
+        async def on_stop():
+            self.stop_called += 1
+
+        task = asynctask.AsyncTaskAsync(main_func, 0.5, on_init, on_stop)
+        task.start()
+        await asyncio.sleep(1)
+        assert task.running()
+        await task.stop()
+
+        assert 0 < self.main_called <= 2
+        assert self.init_called == 1
+        assert self.stop_called == 1
+        assert not task.running()
+
+    @pytest.mark.asyncio
+    async def test_main_exception_skips_iteration(self, mocker):
+        """Test that an exception in the main func only skips current iteration."""
+        self.main_called = 0
+        async def raise_exception():
+            self.main_called += 1
+            raise Exception('something')
+        main_func = raise_exception
+
+        self.init_called = 0
+        async def on_init():
+            self.init_called += 1
+
+        self.stop_called = 0
+        async def on_stop():
+            self.stop_called += 1
+
+        task = asynctask.AsyncTaskAsync(main_func, 0.1, on_init, on_stop)
+        task.start()
+        await asyncio.sleep(1)
+        assert task.running()
+        await task.stop()
+
+        assert 9 <= self.main_called <= 10
+        assert self.init_called == 1
+        assert self.stop_called == 1
+        assert not task.running()
+
+    @pytest.mark.asyncio
+    async def test_on_init_failure_aborts_task(self, mocker):
+        """Test that if the on_init callback fails, the task never runs."""
+        self.main_called = 0
+        async def main_func():
+            self.main_called += 1
+
+        self.init_called = 0
+        async def on_init():
+            self.init_called += 1
+            raise Exception('something')
+
+        self.stop_called = 0
+        async def on_stop():
+            self.stop_called += 1
+
+        task = asynctask.AsyncTaskAsync(main_func, 0.1, on_init, on_stop)
+        task.start()
+        await asyncio.sleep(0.5)
+        assert not task.running() # Since on_init fails, task never starts
+        await task.stop()
+
+        assert self.init_called == 1
+        assert self.stop_called == 1
+        assert self.main_called == 0
+        assert not task.running()
+
+    @pytest.mark.asyncio
+    async def test_on_stop_failure_ends_gacefully(self, mocker):
+        """Test that if the on_init callback fails, the task never runs."""
+        self.main_called = 0
+        async def main_func():
+            self.main_called += 1
+
+        self.init_called = 0
+        async def on_init():
+            self.init_called += 1
+
+        self.stop_called = 0
+        async def on_stop():
+            self.stop_called += 1
+            raise Exception('something')
+
+        task = asynctask.AsyncTaskAsync(main_func, 0.1, on_init, on_stop)
+        task.start()
+        await asyncio.sleep(1)
+        await task.stop()
+        assert 9 <= self.main_called <= 10
+        assert self.init_called == 1
+        assert self.stop_called == 1
+
+    @pytest.mark.asyncio
+    async def test_force_run(self, mocker):
+        """Test that if the on_init callback fails, the task never runs."""
+        self.main_called = 0
+        async def main_func():
+            self.main_called += 1
+
+        self.init_called = 0
+        async def on_init():
+            self.init_called += 1
+
+        self.stop_called = 0
+        async def on_stop():
+            self.stop_called += 1
+            raise Exception('something')
+
+        task = asynctask.AsyncTaskAsync(main_func, 5, on_init, on_stop)
+        task.start()
+        await asyncio.sleep(1)
+        assert task.running()
+        task.force_execution()
+        task.force_execution()
+        await task.stop()
+
+        assert self.main_called == 3
+        assert self.init_called == 1
+        assert self.stop_called == 1
         assert not task.running()
